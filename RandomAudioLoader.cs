6d2ab573647032a5e140f2c2aaa3e09a035171a6
Omar Santiago <omarkenosia@gmail.com>
2021-07-13 10:32:57 -0400

    Add files via upload

---
+++ RandomAudioLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class RandomAudioLoader : MonoBehaviour
+{
+    private AudioSource music = null;
+    private FileInfo[] files = null;
+
+    private void Start()
+    {
+        files = GetResourceFiles("*.mp3");
+
+        int R = Random.Range(0, files.Length);
+        music = CreateaAudioSource(files[R].Name);
+
+        music.Play();
+    }
+    private FileInfo[] GetResourceFiles(string searchPattern)
+    {
+        DirectoryInfo dirInfo = new DirectoryInfo(Application.dataPath + "\\Resources");
+        FileInfo[] files = dirInfo.GetFiles(searchPattern);
+        return files;
+    }
+    private AudioSource CreateAudioSource(string filename)
+    {
+        AudioSource audio = this.gameObject.AddComponent<AudioSource>();
+        audio.clip = Resources.Load<AudioClip>(filename);
+        return audio;
+    }
+}
